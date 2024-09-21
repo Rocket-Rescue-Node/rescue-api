9df362061965556c97eb14b05984b6034c16a34a
snocones.eth <55294825+7or2ga@users.noreply.github.com>
2024-09-21 12:56:29 -0700

    Fix optype URL param check

--- api/messages.go
+++ api/messages.go
@@ -82,6 +82,9 @@ func readJSONRequest(w http.ResponseWriter, r *http.Request, req interface{}) er
 	operatorType, ok := r.URL.Query()["operator_type"]
 	if ok && len(operatorType) > 0 && strings.EqualFold(operatorType[0], "solo") {
 		req.(*CreateCredentialRequest).operatorType = credentials.OperatorType(pb.OperatorType_OT_SOLO)
+	} else {
+		const msg = "missing required URL parameter: operator_type"
+		return &decodingError{status: http.StatusBadRequest, msg: msg}
 	}
 
 	return nil
