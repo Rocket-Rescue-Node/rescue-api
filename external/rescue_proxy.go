d1c304d9125f1ae479d85dd2d95c76ea36b987b8
Adam Gall <galladamj@gmail.com>
2024-10-24 23:13:31 -0400

    Replace deprecated grpc connection code with non deprecated code

--- external/rescue_proxy.go
+++ external/rescue_proxy.go
@@ -34,17 +34,13 @@ func NewRescueProxyAPIClient(logger *zap.Logger, address string, secure bool) *R
 func (c *RescueProxyAPIClient) connect() error {
 	var err error
 
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-
 	// Try to connect to the Rescue Proxy API using TLS.
 	// An empty TLS config will use the system's root CAs.
 	tc := credentials.NewTLS(&tls.Config{})
-	if c.conn, err = grpc.DialContext(ctx,
+	if c.conn, err = grpc.NewClient(
 		c.address,
 		grpc.WithTransportCredentials(tc),
-		grpc.WithBlock()); err == nil {
-
+	); err == nil {
 		c.client = proxy.NewApiClient(c.conn)
 		c.logger.Debug("connected to rescue-proxy with TLS", zap.String("address", c.address))
 		return nil
@@ -58,14 +54,10 @@ func (c *RescueProxyAPIClient) connect() error {
 
 	c.logger.Debug("attempting to connect to rescue-proxy without TLS, since insecure grpc is allowed", zap.String("address", c.address))
 
-	ctx, cancel2 := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel2()
-
-	if c.conn, err = grpc.DialContext(ctx,
+	if c.conn, err = grpc.NewClient(
 		c.address,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithBlock()); err != nil {
-
+	); err != nil {
 		return err
 	}
 
