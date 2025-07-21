6a90fcebe04329c564eb38bfd44e7d6c43a19bbe
Jacob Shufro <jacob@shuf.ro>
2025-07-20 20:50:45 -0400

    Looser signature validation with V byte

--- util/ethereum_test.go
+++ util/ethereum_test.go
@@ -38,3 +38,35 @@ func TestAccountsTextHash(t *testing.T) {
 		})
 	}
 }
+
+func TestRecoverAddressFromSignature(t *testing.T) {
+	tests := []struct {
+		name      string
+		node_id   string
+		msg       string
+		signature string
+	}{
+		{
+			name:      "Example signature",
+			node_id:   "0xEA28d002042fd9898D0Db016be9758eeAFE35C1E",
+			msg:       "Rescue Node 1753055877",
+			signature: "3b1ffab4b818e917b81a878265061660c05a5d9eb55bda623b184d1d8f0af9af58c366d487652a7017d994999ad344f72ea85f2c40ff6da7e0810f8a4a6e7b341c",
+		},
+		{
+			name:      "Example signature with low v",
+			node_id:   "0xEA28d002042fd9898D0Db016be9758eeAFE35C1E",
+			msg:       "Rescue Node 1753055877",
+			signature: "3b1ffab4b818e917b81a878265061660c05a5d9eb55bda623b184d1d8f0af9af58c366d487652a7017d994999ad344f72ea85f2c40ff6da7e0810f8a4a6e7b3401",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			signature, err := hex.DecodeString(tt.signature)
+			assert.NoError(t, err)
+			address, err := RecoverAddressFromSignature([]byte(tt.msg), signature)
+			assert.NoError(t, err)
+			assert.Equal(t, common.HexToAddress(tt.node_id), *address)
+		})
+	}
+}
